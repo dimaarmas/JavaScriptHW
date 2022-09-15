de85ddc91a0bef73cd04e3cfc0f8fa0adfd157e6
dimaa <dimaarmas@gmail.com>
2022-09-15 21:16:19 +0200

    modal functions added

--- navbar/scripts/script.js
+++ navbar/scripts/script.js
@@ -1,6 +1,18 @@
+//NAVBAR
 const navMenuBtn = document.querySelector('.nav-menu-button');
 const navMenu = document.querySelector('.nav-menu');
 
 navMenuBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active')
 });
+
+//Modal Window
+
+const popUpContainer = document.querySelector('.popup');
+const accountBtn = document.querySelector('.account-btn');
+const crossIcon = document.querySelector('.cross-icon');
+
+accountBtn.addEventListener('click', () => popUpContainer.classList.add('visible'));
+
+crossIcon.addEventListener('click', () => popUpContainer.classList.remove('visible'));
+
