6c423d98e4bb89deaf1127173e45be3f51f7adcc
Dumitru Armas <dimaarmas@gmail.com>
2022-09-27 19:41:54 +0200

    fixed homework

--- searchById_homework/scripts/script.js
+++ searchById_homework/scripts/script.js
@@ -1,39 +1,33 @@
 const form = document.querySelector('.form')
-const rootElem = document.querySelector('#root');
+const rootElem = document.querySelector('.root');
 
 
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const render = (json) => {
-        const searchById = json.map(({ name, email }) => {
-            const card = document.createElement('div');
-            const fullNameElem = document.createElement('p');
-            const emailElem = document.createElement('a');
-
-            fullNameElem.innerText = name;
-            emailElem.innerText = email;
-
-            emailElem.href = `mailto:${email}`;
-
-            card.append(fullNameElem, emailElem)
-            return card
-        })
-
-        rootElem.append(...searchById);
-
-    }
-
-    const { id } = event.target
-
-    fetch(`https://jsonplaceholder.typicode.com/users${id.value}`)
+const getUser = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then(resp => resp.json())
         .then(json => render(json))
+}
 
+getUser();
+
+
+const render = (data) => {
+    rootElem.innerText = '';
+    const card = document.createElement('div');
+    const fullNameElem = document.createElement('p');
+    const emailElem = document.createElement('a');
+
+    fullNameElem.innerText = data.name;
+    emailElem.innerText = data.email;
+
+    emailElem.href = `mailto:${data.email}`;
+
+    card.append(fullNameElem, emailElem)
+    rootElem.append(card)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const id = event.target.id.value;
+    getUser(id);
 })
-
-
-
-
