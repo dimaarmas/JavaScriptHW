c04b31fce72b9c95c764fc6ed89a80c9fadd72d1
Dumitru Armas <dimaarmas@gmail.com>
2022-10-13 18:58:43 +0200

    navbar created

---
+++ Lesson_React_13_10/new_app/src/components/Navbar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from './media/buh_one_logo.png'
+import s from './index.module.css'
+
+export default function Navbar() {
+    return (
+        <nav className={['wrapper', s.nav_block].join(' ')}>
+            <a href="#">
+                <img alt="company_logo" src={logo} />
+            </a>
+
+            <ul className={s.nav_menu}>
+                <li>Главное</li>
+                <li>Услуги</li>
+                <li>Кейсы</li>
+                <li>О компании</li>
+                <li>Контакты</li>
+            </ul>
+        </nav>
+    )
+}
