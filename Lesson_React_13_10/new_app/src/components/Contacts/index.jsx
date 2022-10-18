2d67de206c33312f59b149d75d131ccf140aff21
Dumitru Armas <dimaarmas@gmail.com>
2022-10-18 20:29:26 +0200

    contact section added

---
+++ Lesson_React_13_10/new_app/src/components/Contacts/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from './index.module.css'
+import { MobileOutlined, MailOutlined } from '@ant-design/icons'
+
+export default function Contact() {
+    return (
+        <section className={s.sections}>
+            <div className={['wrapper', s.contacts_block].join(' ')}>
+                <div className={s.info_block}>
+                    <h2>Связь с нами</h2>
+                    <p>У вас остались вопросы? Напишите нам - мы ответим в ближайшее время!</p>
+                    <div className={s.main_contacts}>
+                        <p><MobileOutlined /> +7 (111) 222-33-44</p>
+                        <p><MailOutlined /> order@bu-one.ru</p>
+                    </div>
+                </div>
+                <div className={s.form_block}></div>
+            </div>
+        </section>
+
+    )
+}
