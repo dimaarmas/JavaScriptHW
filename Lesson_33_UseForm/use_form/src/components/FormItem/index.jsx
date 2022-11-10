ebe4d4d7ed7c2c8f33d1e592d280d4657476c77f
Dumitru Armas <dimaarmas@gmail.com>
2022-11-10 20:35:27 +0100

    form validation finished

--- Lesson_33_UseForm/use_form/src/components/FormItem/index.jsx
+++ Lesson_33_UseForm/use_form/src/components/FormItem/index.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import FormButton from '../FormButton'
 import FormInput from '../FormInput'
 import s from './index.module.css'
+import { useForm } from 'react-hook-form'
 
 export default function FormItem({ title, button, infoText, infoText_2, formType }) {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onBlur' });
+
+    const submit = (data) => console.log(data)
+
+    const emailRegister = register('email', {
+        required: '* Enter your email',
+        pattern: {
+            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            message: '*Not valid email format'
+        }
+    });
+    const passwordRegister = register('password', {
+        required: '* Enter your password',
+        pattern: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+            message: '*Your password should have minimum eight characters, at least one letter, one number and one special character'
+        }
+    });
+
     return (
-        <form className={s.form_item}>
+        <form className={s.form_item} onSubmit={handleSubmit(submit)}>
             <p className={s.form_title}>{title}</p>
 
             <p className={s.form_info}>{infoText_2}</p>
@@ -14,7 +35,12 @@ export default function FormItem({ title, button, infoText, infoText_2, formType
                 type='text'
                 name='email'
                 placeholder='Email'
+                {...emailRegister}
             />
+
+            <div className={s.error_message}>
+                {errors.email && <p>{errors.email?.message}</p>}
+            </div>
 
             {
                 ['registration', 'login'].includes(formType)
@@ -22,9 +48,19 @@ export default function FormItem({ title, button, infoText, infoText_2, formType
                         type='text'
                         name='password'
                         placeholder='Password'
+                        {...passwordRegister}
                     />
                     : ''
             }
+
+            {
+                ['registration', 'login'].includes(formType)
+                    ? <div className={s.error_message}>
+                        {errors.password && <p>{errors.password?.message}</p>}
+                    </div>
+                    : ''
+            }
+
 
 
 
