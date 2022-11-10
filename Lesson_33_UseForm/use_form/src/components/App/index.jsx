93a6c9002c6a1711f17f5cd6a3a1853db8f77dec
Dumitru Armas <dimaarmas@gmail.com>
2022-11-10 19:03:34 +0100

    form_item finished

--- Lesson_33_UseForm/use_form/src/components/App/index.jsx
+++ Lesson_33_UseForm/use_form/src/components/App/index.jsx
@@ -3,7 +3,24 @@ import FormItem from "../FormItem";
 function App() {
   return (
     <div>
-      <FormItem />
+      <FormItem
+        title='Registration'
+        button={{ submit: 'Registration', redirect: 'Login' }}
+        infoText={'By registering on the site, you agree to our Rules and Privacy Policy and agree to receive newsletters.'}
+        formType={'registration'}
+      />
+      <FormItem
+        title='Login'
+        button={{ submit: 'Login', redirect: 'Registration' }}
+        infoText={'Reset password'}
+        formType={'login'}
+      />
+      <FormItem
+        title='Reset password'
+        button={{ submit: 'Send' }}
+        infoText={'The temporary password is valid for 24 hours.'}
+        infoText_2={'To receive a temporary password, you must enter the email address you provided during registration.'}
+      />
     </div>
   );
 }
--- Lesson_33_UseForm/use_form/src/components/FormItem/index.jsx
+++ Lesson_33_UseForm/use_form/src/components/FormItem/index.jsx
@@ -3,25 +3,39 @@ import FormButton from '../FormButton'
 import FormInput from '../FormInput'
 import s from './index.module.css'
 
-export default function FormItem() {
+export default function FormItem({ title, button, infoText, infoText_2, formType }) {
     return (
         <form className={s.form_item}>
-            <p className={s.form_title}>Registration</p>
+            <p className={s.form_title}>{title}</p>
+
+            <p className={s.form_info}>{infoText_2}</p>
+
             <FormInput
                 type='text'
                 name='email'
                 placeholder='Email'
             />
 
-            <FormInput
-                type='text'
-                name='password'
-                placeholder='Password'
-            />
+            {
+                ['registration', 'login'].includes(formType)
+                    ? <FormInput
+                        type='text'
+                        name='password'
+                        placeholder='Password'
+                    />
+                    : ''
+            }
 
-            <p className={s.form_info}>By registering on the site, you agree to our Rules and Privacy Policy and agree to receive newsletters.</p>
-            <FormButton color='yellow'>Registration</FormButton>
-            <FormButton color='white'>Login</FormButton>
+
+
+            <p className={s.form_info}>{infoText}</p>
+            <FormButton color='yellow'>{button.submit}</FormButton>
+
+            {
+                ['registration', 'login'].includes(formType)
+                    ? <FormButton color='white'>{button.redirect}</FormButton>
+                    : ''
+            }
         </form>
     )
 }
