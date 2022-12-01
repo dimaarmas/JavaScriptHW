093ee948320155af277f10729ca101249d0a067a
Dumitru Armas <dimaarmas@gmail.com>
2022-12-01 20:57:55 +0100

    localStorage added project upds

--- Lesson_31_teams_master/teams_master/src/components/App/index.jsx
+++ Lesson_31_teams_master/teams_master/src/components/App/index.jsx
@@ -2,13 +2,25 @@ import TeamsPage from "../../pages/TeamsPage";
 import ConfigurationsPage from "../../pages/ConfigurationsPage";
 import Navbar from "../Navbar";
 import { Routes, Route } from 'react-router-dom'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Context } from "../../context";
 
 function App() {
 
   const [teams, setTeams] = useState([]);
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const teams = JSON.parse(localStorage.getItem('teams'));
+    const users = JSON.parse(localStorage.getItem('users'));
+    if (teams) setTeams(teams);
+    if (users) setUsers(users);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('teams', JSON.stringify(teams));
+    localStorage.setItem('users', JSON.stringify(users));
+  }, [teams, users])
 
   const add_team = (team_value) => {
     setTeams([...teams, { value: team_value, label: team_value }])
