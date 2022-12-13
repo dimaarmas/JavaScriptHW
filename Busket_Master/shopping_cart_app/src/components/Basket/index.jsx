851963543e1328d13c966ff4c9e83b391a4088cb
Dumitru Armas <dimaarmas@gmail.com>
2022-12-13 21:01:01 +0100

    increment and decrement added

--- Busket_Master/shopping_cart_app/src/components/Basket/index.jsx
+++ Busket_Master/shopping_cart_app/src/components/Basket/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { removeBasket } from '../../store/reducer/basketReducer'
+import { useDispatch } from 'react-redux'
 import BasketCards from '../BusketCards'
 
-export default function Basket() {
+export default function Basket({ id, title, price }) {
 
     const state = useSelector(state => state.basket)
+
+    const dispatch = useDispatch();
+
+    const deleteBasket = () => dispatch(removeBasket({ id, title, price }))
 
     return (
         <div>
             {
-                state.map(el => <BasketCards key={el.id} {...el} />)
+                state.length !== 0
+                    ? <div>
+                        <div>
+                            {state.map(el => <BasketCards key={el.id} {...el} />)}
+                        </div>
+                        <button onClick={deleteBasket}>Clear Basket</button>
+                    </div>
+                    : ''
             }
         </div>
     )
