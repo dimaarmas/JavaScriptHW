b3fec167d6d262e04d1e242121ed72403f26dd42
Dumitru Armas <dimaarmas@gmail.com>
2022-12-20 21:57:21 +0100

    thunk added

---
+++ Lesson_39_FetchRedux/fetch_request_redux/src/pages/MainPage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProductsContainer from '../../components/ProductsContainer'
+
+export default function MainPage() {
+    return (
+        <div><ProductsContainer /></div>
+    )
+}
