9c514f1f8ea2df3c9d33cf8f3eb56832d589e70d
Dumitru Armas <dimaarmas@gmail.com>
2023-01-12 21:55:21 +0100

    cart page started

--- Lesson_40_preProject/pre_project_app/src/store/index.js
+++ Lesson_40_preProject/pre_project_app/src/store/index.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { categoriesReducer } from './reducers/categories';
 import { productReducer } from './reducers/product';
 import { productsReducer } from './reducers/products';
+import { cartReducer } from './reducers/cart';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     category: categoriesReducer,
     product: productReducer,
-    products: productsReducer
+    products: productsReducer,
+    cart: cartReducer
 
 });
 
