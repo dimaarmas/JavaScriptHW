f57f1c82e44913b9890d0b04b4d13f32ffe4a977
Dumitru Armas <dimaarmas@gmail.com>
2023-01-12 21:53:06 +0100

    product page finished

--- Lesson_40_preProject/pre_project_app/src/components/ProductCard/index.jsx
+++ Lesson_40_preProject/pre_project_app/src/components/ProductCard/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import s from './index.module.css'
 
-export default function ProductCard({ title, image }) {
+export default function ProductCard({ id, title, image }) {
+
+    const product_url = `/product/${id}`
+
     return (
         <div className={s.product_card}>
-            <img src={image} alt={title} />
+            <Link to={product_url}>
+                <img src={image} alt={title} />
+            </Link>
             <p>Title: {title} </p>
-            <button>Add to cart</button>
+            <Link to={product_url}>
+                <button>Add to cart</button>
+            </Link>
         </div>
     )
 }
--- Lesson_40_preProject/pre_project_app/src/pages/ProductPage/index.jsx
+++ Lesson_40_preProject/pre_project_app/src/pages/ProductPage/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { load_all_products } from '../../store/requests/product_request';
+import s from './index.module.css'
 
 export default function ProductPage() {
 
@@ -14,10 +15,22 @@ export default function ProductPage() {
         dispatch(load_all_products(id))
     }, [])
 
-    console.log(state);
+    const { title, description, price, image } = state;
 
 
     return (
-        <div>ProductPage {id}</div>
+        <div className={s.product_item}>
+            <img src={image} alt={title} />
+            <div className={s.info_block}>
+                <p className={s.title}>{title}</p>
+                <p className={s.description}>{description}</p>
+
+                <div className={s.price_block}>
+                    <p><span>Price:</span>{price} $</p>
+                    <button>Add to Cart</button>
+                </div>
+
+            </div>
+        </div>
     )
 }
