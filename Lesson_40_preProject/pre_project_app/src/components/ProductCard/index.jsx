0d65f73c1600b8c598d24d502d7be1c9e159d32f
Dumitru Armas <dimaarmas@gmail.com>
2023-01-17 21:58:57 +0100

    cart page started

--- Lesson_40_preProject/pre_project_app/src/components/ProductCard/index.jsx
+++ Lesson_40_preProject/pre_project_app/src/components/ProductCard/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import s from './index.module.css'
+import { addToCart } from '../../store/reducers/cart'
+import { useDispatch } from 'react-redux'
 
 export default function ProductCard({ id, title, image, price }) {
 
     const product_url = `/product/${id}`
 
+    const dispatch = useDispatch();
+
+    const add_to_cart = () => dispatch(addToCart({ id, title, image, price }));
+
     return (
         <div className={s.product_card}>
             <Link to={product_url}>
                 <img src={image} alt={title} />
+                <p>Title: {title} </p>
+                <p>Price: {price} $</p>
             </Link>
-            <p>Title: {title} </p>
-            <p>Price: {price} $</p>
-            <Link to={product_url}>
-                <button>Add to cart</button>
-            </Link>
+            <button onClick={add_to_cart}>Add to cart</button>
+
         </div>
     )
 }
